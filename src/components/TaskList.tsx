319fc8a085c20febbeffc0bf4a09599d1ec0c19f
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-02-22 23:19:56 -0300

    feat: add task list component

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,33 +1,102 @@
+import { PlusCircle } from '@phosphor-icons/react'
+import { ChangeEvent, FormEvent, useState } from 'react'
+import { TaskCounter } from './TaskCounter'
+import { Empty } from './Empty'
 import styles from './TaskList.module.css'
+import { Task, ITask } from './Task'
 
 export function TaskList() {
-  return (
-    <div className={styles.taskList}>
-      <aside>
-        <p
-          className={styles.createdTasks}
-        >
-          Tarefas criadas
-        </p>
-        <span
-          className={styles.counters}
-        >
-          0
-        </span>
-      </aside>
+  const [tasks, setTasks] = useState<ITask[]>([])
+  const [newTaskText, setNewTaskText] = useState('')
 
-      <aside>
-        <p
-          className={styles.completedTasks}
+  function calculateNumberCompletedTasks(tasks: ITask[]) {
+    return tasks.filter(task => task.isDone).length
+  }
+
+  function removeTask(taskId: number) {
+    const tasksWithoutRemoveOne = tasks.filter(task => {
+      return task.id !== taskId
+    })
+    setTasks(tasksWithoutRemoveOne)
+  }
+
+  function completeTask(taskId: number) {
+    const completedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        return { ...task, isDone: task.isDone ? false : true }
+      }
+
+      return task
+    })
+
+    setTasks(completedTasks)
+  }
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value)
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+    setTasks([
+      ...tasks,
+      {
+        id: Math.floor(Math.random() * 1000),
+        text: newTaskText,
+        isDone: false,
+      },
+    ])
+    setNewTaskText('')
+  }
+
+  return (
+    <article>
+      <header className={styles.task}>
+        <form
+          className={styles.taskForm}
+          onSubmit={handleCreateNewTask}
         >
-          Concluídas
-        </p>
-        <span
-          className={styles.counters}
-        >
-          0
-        </span>
-      </aside>
-    </div>
+          <input
+            type='text'
+            name='task'
+            placeholder='Adicione uma nova tarefa'
+            value={newTaskText}
+            onChange={handleNewTaskChange}
+            required
+          />
+          <button
+            type='submit'
+          >
+            <span>
+              Criar <PlusCircle size={16} color='#F2F2F2' weight='bold' />
+            </span>
+          </button>
+        </form>
+      </header>
+
+      <TaskCounter
+        createdTask={tasks.length}
+        completedTask={calculateNumberCompletedTasks(tasks)}
+      />
+
+      {
+        tasks.length ?
+          <>
+            {
+              tasks.map(task => {
+                return (
+                  <Task
+                    key={task.id}
+                    task={task}
+                    onRemoveTask={removeTask}
+                    onCompleteTask={completeTask}
+                  />
+                )
+              })
+            }
+          </> : <Empty />
+      }
+
+    </article>
   )
 }
